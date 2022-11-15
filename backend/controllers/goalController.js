081f8d76a65d014e0117ee5a425f06a48804ae2b
eleeyeah <ilietmks@gmail.com>
2022-11-15 17:01:08 +0100

    setting and updating user goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 
 
@@ -8,7 +9,7 @@ const Goal = require('../models/goalModel');
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({ user: req.user.id });
 
     res.status(200).json(goals);
 })
@@ -23,7 +24,8 @@ const setGoal = asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     });
 
     res.status(200).json(goal);
@@ -39,6 +41,22 @@ const updateGoal = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Goal not found');
     }
+
+    const user = await User.findById(req.user.id);
+
+    //Check for user
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    //Check if user is owner of goal
+    if (goal.user.toString() !== user._id.toString()) {
+        res.status(401);
+        throw new Error('User Not authorized');
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
     })
@@ -57,6 +75,21 @@ const deleteGoal = asyncHandler(async (req, res) => {
     if (!goal) {
         res.status(400);
         throw new Error('Goal not found');
+    }
+
+    const user = await User.findById(req.user.id);
+
+    //Check for user
+
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    //Check if user is owner of goal
+    if (goal.user.toString() !== user._id.toString()) {
+        res.status(401);
+        throw new Error('User Not authorized');
     }
 
     await goal.remove();
