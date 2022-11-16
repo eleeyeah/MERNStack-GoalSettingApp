642ed83735768485ba53bb52970362752b14f4fd
eleeyeah <ilietmks@gmail.com>
2022-11-16 20:19:19 +0100

    Login hooked-up

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -15,6 +15,18 @@ const register = async (userData) => {
     return response.data;
 }
 
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData);
+
+    if (response.data.accessToken) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+    }
+
+    return response.data;
+}
+
+
 //Logout user
 const logout = () => {
     localStorage.removeItem('user');
@@ -23,7 +35,8 @@ const logout = () => {
 
 const authService = {
     register,
-    logout
+    logout,
+    login
 }
 
 export default authService;
